afd11552e5eb421f1dc88a60c220541e5d65e12b
kelvin.man <wenweixin.maxman@foxmail.com>
2025-04-01 10:08:06 +0800

    使用due中的fiber模块

--- cmd/webclient/main.go
+++ cmd/webclient/main.go
@@ -1,21 +1,36 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"github.com/dobyte/due/component/http/v2"
+	"github.com/dobyte/due/v2"
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/static"
 )
 
 func main() {
+	// 创建容器
+	container := due.NewContainer()
+
+	fiberComponent := http.NewServer(
+		http.WithAddr(":8081"),
+	)
+
+	router := fiberComponent.Proxy().Router()
 	// 设置静态文件目录
-	fs := http.FileServer(http.Dir("web_resource/dist/"))
+	router.Get("/*", static.New("./web_resource/dist"))
 
-	// 设置路由，将所有 HTTP 请求转发给文件服务器
-	http.Handle("/", fs)
+	// 设置路由
+	router.Get("/hello", func(c fiber.Ctx) error {
+		name := c.Query("name", "world")
+		c.JSON(map[string]string{
+			"message": "hello " + name,
+		})
+		return nil
+	})
 
-	// 监听并在 8089 端口启动服务器
-	log.Println("Listening on :8089...")
-	err := http.ListenAndServe(":8089", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// 添加组件
+	container.Add(fiberComponent)
+
+	// 启动容器
+	container.Serve()
 }
